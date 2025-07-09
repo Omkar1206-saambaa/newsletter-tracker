657473b4b766cf9c34bdf4bd9b0567267fb783df
Omkar1206-saambaa <omkar@saambaa.com>
2025-07-09 15:51:50 +0530

    Update redirect.js

--- pages/redirect.js
+++ pages/redirect.js
@@ -1,6 +1,10 @@
 export async function getServerSideProps(context) {
-  const { vertical, campaign, syndicator, target } = context.query;
-  const decodedTarget = decodeURIComponent(target || "");
+  const { vertical = "", campaign = "", syndicator = "", target } = context.query;
+
+  // Use hardcoded SendStak link if no `target` param is present
+  const decodedTarget = decodeURIComponent(
+    target || "https://sendstak.com/redirect/vppthsjYl-?user={{ contact.EMAIL }}"
+  );
 
   return {
     props: {
@@ -17,22 +21,26 @@ export default function Redirect({ vertical, campaign, syndicator, target }) {
     <html>
       <head>
         <title>Redirecting...</title>
+
         {/* Google Analytics */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-0SLX5YWDZD"
         ></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0SLX5YWDZD', {
-              page_path: '/${vertical}/${campaign}/${syndicator}',
-            });
-          `,
-        }}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0SLX5YWDZD', {
+                page_path: '/${vertical}/${campaign}/${syndicator}',
+              });
+            `,
+          }}
+        />
 
+        {/* Meta Redirect */}
         <meta httpEquiv="refresh" content={`0; URL='${target}'`} />
       </head>
       <body>
